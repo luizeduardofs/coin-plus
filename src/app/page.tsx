878b67c1b8801e7d442654d06bee1ca8f7d7e7c7
luizeduardofs <edu.dev.py@gmail.com>
2025-12-26 16:56:50 -0300

    Implement home components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,31 @@
+import { Suspense } from "react";
+
+import { Categories } from "@/components/home/Categories";
+import { CoinOverview } from "@/components/home/CoinOverview";
+import { TrendingCoins } from "@/components/home/TrendingCoins";
+import {
+  CategoriesFallback,
+  CoinOverviewFallback,
+  TrendingCoinsFallback,
+} from "@/components/home/fallback";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <main className="main-container">
+      <section className="home-grid">
+        <Suspense fallback={<CoinOverviewFallback />}>
+          <CoinOverview />
+        </Suspense>
+
+        <Suspense fallback={<TrendingCoinsFallback />}>
+          <TrendingCoins />
+        </Suspense>
+      </section>
+      <section className="w-full mt-7 space-y-4">
+        <Suspense fallback={<CategoriesFallback />}>
+          <Categories />
+        </Suspense>
+      </section>
+    </main>
+  );
 }
